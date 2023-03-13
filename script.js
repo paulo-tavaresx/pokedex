d0c94a60d79afb623a175f9b037e0aa6bd9f5caf
Paulo Tavares <92366425+pofft@users.noreply.github.com>
2023-03-13 00:44:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
   const cardsAvailable = [
     {
       pokemonName: 'Bulbasaur',
-      img: '../asserts/imgs/bulbassur.png',
+      img: 'blob/main/asserts/imgs/aron.png',
       id: 1,
       types: ['grass', 'poison'],
       data: {
